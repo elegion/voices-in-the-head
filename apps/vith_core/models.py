ab92f56a02646d755ae2d2fc8366f0a8cd2713cd
Dmitry Medvinsky <dmitry.medvinsky@gmail.com>
2010-08-15 03:13:15 +0400

    Code style

--- apps/vith_core/models.py
+++ apps/vith_core/models.py
@@ -1,4 +1,5 @@
-import datetime, os
+import datetime
+import os
 
 from django.conf import settings
 from django.db import models
@@ -9,34 +10,37 @@ from core import datetime_to_timestamp
 from core.dbfields import AudioFileField
 
 
-MAX_LENGTH = getattr(settings, 'MAX_TRACK_LENGTH', 5*60)
+MAX_LENGTH = getattr(settings, 'MAX_TRACK_LENGTH', 5 * 60)
 NORMALIZE = getattr(settings, 'TRACK_NORMALIZE', -6)
 
 
 class Uploader(models.Model):
     twitter = models.CharField(max_length=250)
-    
+
     def __unicode__(self):
         return '@%s' % self.twitter
-    
-    
+
+
 class Track(models.Model):
-    track_file = AudioFileField(format='mp3', bitrate=196, normalize=NORMALIZE, max_length=MAX_LENGTH,
-        upload_to=os.path.join(settings.WRITABLE_FOLDER, 'tracks'))
+    track_file = AudioFileField(format='mp3', bitrate=196,
+                                normalize=NORMALIZE,
+                                max_length=MAX_LENGTH,
+                                upload_to=os.path.join(settings.WRITABLE_FOLDER, 'tracks'))
     length = models.PositiveSmallIntegerField() #seconds
     name = models.CharField(max_length=250)
     uploader = models.ForeignKey(Uploader, null=True)
-    
+
     play_time = models.DateTimeField(null=True, blank=True)
     uploaded = models.DateTimeField(null=True, blank=True, auto_now_add=True)
-        
+
     class Meta(object):
         get_latest_by = 'play_time'
         ordering = ['play_time', 'uploaded']
-        
+
     def __unicode__(self):
-        return '%(name)s, %(length)ss from %(uploader)s' % {'name': self.name, 'length': self.length,\
-            'uploader': self.uploader}
+        return '%(name)s, %(length)ss from %(uploader)s' % {'name': self.name,
+                                                            'length': self.length,
+                                                            'uploader': self.uploader}
 
     def as_dict(self):
         tdict = model_to_dict(self)
@@ -51,7 +55,7 @@ class Track(models.Model):
         """
         if not self.length and self.track_file:
             self.length = self.track_file._duration
-            
+
         if not self.play_time:
             try:
                 last = Track.objects.latest('play_time')
@@ -60,5 +64,5 @@ class Track(models.Model):
                 self.play_time = last.play_time + datetime.timedelta(seconds=last.length)
             except Track.DoesNotExist:
                 self.play_time = datetime.datetime.now()
-            
+
         super(Track, self).save(*args, **kwargs)
--- apps/vith_core/views.py
+++ apps/vith_core/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from core import JsonResponse, datetime_to_timestamp
+from core import JsonResponse
 from vith_core.forms import UploadForm
 from vith_core.models import Track
 
@@ -10,7 +10,7 @@ def tracks(request):
     Return list of all uploaded tracks in json format
     """
     tracks = Track.objects.filter(play_time__gte=datetime.datetime.now())
-    
+
     return JsonResponse([t.as_dict() for t in tracks])
 
 
@@ -18,22 +18,22 @@ def now_playing(request):
     """
     Returns current track and now playing position
     FIXME synchronize with audio streamer
-    """    
+    """
     now = datetime.datetime.now()
     curr_track = Track.objects.filter(play_time__lte=now)\
         .order_by('-play_time')[0]
     if curr_track:
         curr_pos = (now - curr_track.play_time).seconds
-        
+
     if curr_pos > curr_track.length:
         data = []
     else:
         tdata = curr_track.as_dict()
         tdata['position'] = curr_pos
         data = [tdata]
-    
+
     return JsonResponse(data)
-    
+
 
 def upload(request):
     """
@@ -44,12 +44,8 @@ def upload(request):
         track = form.save(commit=False)
         track.length = 0
         track.save()
-        return JsonResponse({
-            'status': 'ok',
-            'track': track.as_dict()
-        })
-    
-    return JsonResponse({
-        'status': 'error',
-        'errors': dict(form.errors)
-    })
+        return JsonResponse({'status': 'ok',
+                             'track': track.as_dict()})
+
+    return JsonResponse({'status': 'error',
+                         'errors': dict(form.errors)})
