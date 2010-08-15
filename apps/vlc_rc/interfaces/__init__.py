db2a93a1a20492d716682e15429bb1194d7f2045
Dmitry Medvinsky <dmitry.medvinsky@gmail.com>
2010-08-15 04:14:21 +0400

    Fix vlc_rc module imports
    Didn't work, when used from other apps.

--- apps/vlc_rc/interfaces/__init__.py
+++ apps/vlc_rc/interfaces/__init__.py
@@ -3,12 +3,13 @@ class InvalidInterfaceException(Exception):
 
 
 def get_by_name(name):
-    module_name = '%s.%s' % ('interfaces', name)
+    module_name = '%s.%s.%s' % ('vlc_rc', 'interfaces', name)
     try:
         module = __import__(module_name)
     except ImportError:
         raise InvalidInterfaceException
     try:
+        module = getattr(module, 'interfaces')
         return getattr(module, name).Interface
     except AttributeError:
         raise InvalidInterfaceException
--- apps/vlc_rc/tests.py
+++ apps/vlc_rc/tests.py
@@ -1,17 +1,17 @@
 import unittest
 
-import vlc_rc
+import rc
 
 
 class FactoryTestCase(unittest.TestCase):
     def test_telnet(self):
-        iface = vlc_rc.VlcRC().get(vlc_rc.INTERFACE_TELNET)
+        iface = rc.VlcRC().get(rc.INTERFACE_TELNET)
         from interfaces.telnet import Interface
         self.assertEquals(Interface, iface)
 
     def test_invalid(self):
-        self.assertRaises(vlc_rc.interfaces.InvalidInterfaceException,
-                          vlc_rc.VlcRC().get, 'telephaty')
+        self.assertRaises(rc.interfaces.InvalidInterfaceException,
+                          rc.VlcRC().get, 'telephaty')
 
 
 if __name__ == '__main__':
